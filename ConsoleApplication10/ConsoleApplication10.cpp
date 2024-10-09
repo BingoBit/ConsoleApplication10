b9e18d98e6699e315caa41b94d2ae2a7ca514279
Lenovo user <Lenovo user@DESKTOP-R3F6B6E>
2024-10-09 21:26:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    const int stro = 3; 
+    const int stl = 3; 
+    int arr[stro][stl] = {
+        {1, -2, 3,},
+        {4, 5, -6,},
+        {7, -8, 9}
+    };
+
+    int sum = 0;
+    int we = stro * stl;
+
+    for (int i = 0; i < stro; ++i)
+    {
+        for (int j = 0; j < stl; ++j)
+        {
+            sum += arr[i][j];
+
+            if (arr[i][j] > 0)
+            {
+                cout << "Положительные =\n" << (arr[i][j]);
+            }
+            else if (arr[i][j] < 0)
+            {
+                cout << "Отрицательные =\n" << (arr[i][j]);
+            }
+        }
+    }
+    int sred = sum / we;
+    cout << "Сумма = \n" << sum << endl;
+    cout << "Среднее = \n" << sred << endl;
+
+}
